a1096667814906621c81cbee51a948091f18c451
SeraphielSpark <taiwoemmanuel435@gmail.com>
2025-12-06 23:50:02 -0800

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
-      base:"/port",
+      base:"/pipeline",
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
